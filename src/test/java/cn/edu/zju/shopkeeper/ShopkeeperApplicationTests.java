2cdc0d58b9d5b21de8563f411914e648edbd4554
王泽杰 <wangzj@uubee.com>
2018-07-15 09:39:56 +0800

    初始化

---
+++ src/test/java/cn/edu/zju/shopkeeper/ShopkeeperApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.edu.zju.shopkeeper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopkeeperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
